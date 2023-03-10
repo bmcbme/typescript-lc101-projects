ef143924a3e7d1e1a8c5d253e82f5aa0ea7d92b3
bmcbme <beccamcbme10@ymail.comgit config --global user.email you@example.com>
2023-03-09 19:15:23 -0600

    test

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -4,28 +4,56 @@
 
 // Part 1: Declare (5) Variables With Type
 
+let kilometersToMars: number = 225000000;
+let kilometersToTheMoon: number = 384400;
+
+
 
 
 // Part 2: Print Days to Mars
-
+let milesToMars: number = kilometersToMars * milesPerKilometer
+let hoursToMars: number = milesToMars / speedMph
+let daysToMars: number = hoursToMars / 24
 
 
 // Code an output statement here (use a template literal):
-
+console.log(`${spacecraftName} would take ${daysToMars} days to get to Mars.`)
 
 
 // Part 3: Create a Function ("getDaysToLocation")
-
+function getDaysToLocation(kilometersAway: number): number {
+    let milesAway: number = kilometersAway * milesPerKilometer;
+    let hours: number = milesAway / speedMph;
+    return hours / 24;
+ }
 
 
 // Move your output statement from part 2 here. Update the template literal to call
 // the function and print the outputs for a Mars trip and a moon trip.
-
-
+console.log(`${spacecraftName} would take ${getDaysToLocation(kilometersToMars)} days to get to Mars.`)
+console.log(`${spacecraftName} would take ${getDaysToLocation(kilometersToTheMoon)} days to get to the Moon.`)
 
 
 // Part 4: Create a Spacecraft Class
-
+class Spacecraft {
+    name: string = 'Determination'
+    speedMph: number = 17500
+    milesPerKilometer: number = 0.621;
+ 
+    constructor(name: string, speedMph: number) {
+       this.name = name;
+       this.speedMph = speedMph;
+    }
+ 
+    getDaysToLocation(kilometersAway: number): number {
+       let milesAway: number = kilometersAway * this.milesPerKilometer;
+       let hoursToMars: number = milesAway / this.speedMph;
+       return hoursToMars / 24;
+    }
+    printDaysToLocation(location: SpaceLocation) {
+        console.log(`${this.name} would take ${this.getDaysToLocation(location.kilometersAway)} days to get to ${location.name}.`);
+     }
+ }
 
 
 
